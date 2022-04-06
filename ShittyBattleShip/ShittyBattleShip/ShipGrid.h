1761a877e471292f683f50b9d97a20c02099f142
Xavier S <spartan9077@gmail.com>
2022-04-06 10:54:07 -0600

    fixed folder upload structure

--- ShittyBattleShip/ShittyBattleShip/ShipGrid.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Grid.h"
-class ShipGrid : public Grid
-{
-public:
-	void FillShipGridWithShips();
-	void FireTorpedoAtShipGrid(int xShot, int yShot);
-	int ShipHit(Point p);
-};
